67ef38c86f99a88611d8532ba7e6dbf0129f752d
tvconss <tranvanconkg@gmail.com>
2026-02-25 14:26:38 +0700

    optimize (app): use env util to get port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { EnvUtil } from './common/utils/env.util';
+import { EnvKey } from './configs/env.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = EnvUtil.getEnv(EnvKey.PORT, false, 3000);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT);
 }
 
 bootstrap();
