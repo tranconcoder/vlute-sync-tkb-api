507e35f18b33aac3143c83f39e83ff99c5994398
tvconss <tranvanconkg@gmail.com>
2026-02-27 10:28:16 +0700

    refactor(vlute): move hardcoded browser headers to configuration

--- src/modules/vlute/login/login.service.ts
+++ src/modules/vlute/login/login.service.ts
@@ -47,20 +47,7 @@ export class LoginService {
         maxRedirects: 5,
         validateStatus: (status) => status === 200 || status === 302,
         headers: {
-          accept:
-            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-          'accept-language': 'vi,en-US;q=0.9,en;q=0.8',
-          'cache-control': 'max-age=0',
-          priority: 'u=0, i',
-          'sec-ch-ua':
-            '"Not:A-Brand";v="99", "Google Chrome";v="145", "Chromium";v="145"',
-          'sec-ch-ua-mobile': '?0',
-          'sec-ch-ua-platform': '"Linux"',
-          'sec-fetch-dest': 'document',
-          'sec-fetch-mode': 'navigate',
-          'sec-fetch-site': 'same-site',
-          'sec-fetch-user': '?1',
-          'upgrade-insecure-requests': '1',
+          ...this.authConf.sso.initialHeaders,
           'user-agent': this.config.userAgent,
         },
       });
